3366d21cdc02d63b6977f26751dac28269029e4a
Amanda <amanda.ausejo@gmail.com>
2024-03-12 17:00:41 +0100

    implementing redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
+import store from "./app/store";
 import App from "./components/App";
 
 const theme = createTheme({});
